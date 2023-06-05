6c059283b055fe6a5595c602b349a42a5a1063e1
Файзрахманов Евгений Валерьевич <zhenya.f@pyrobyte.ru>
2023-06-05 21:05:54 +0700

    feat: post response

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,12 +13,24 @@ const addresses = [
   { id: 2, title: "Nikitina" },
 ];
 
+const parserMiddleware = bodyParser({});
+app.use(parserMiddleware);
+
 app.get("/products", (req: Request, res: Response) => {
   req.query.title
     ? res.send(
         products.filter((p) => p.title.includes(String(req.query.title)))
       )
     : res.send(products);
+});
+
+app.post("/products", (req: Request, res: Response) => {
+  const newProduct = {
+    id: Number(new Date()),
+    title: req.body.title,
+  };
+  products.push(newProduct);
+  res.status(201).send(newProduct);
 });
 
 app.delete("/products/:id", (req: Request, res: Response) => {
