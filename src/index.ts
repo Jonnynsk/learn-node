569e45942197f7b02b22ab40d5a1f693501c88c3
Файзрахманов Евгений Валерьевич <zhenya.f@pyrobyte.ru>
2023-06-12 16:58:12 +0700

    feat: middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import bodyParser from "body-parser";
 
 import { AddressesRouter } from "./routers/addresses-router";
@@ -7,12 +7,20 @@ import { ProductRouter } from "./routers/products-router";
 const app = express();
 const port = process.env.PORT || 3000;
 
+let requestCounter = 0;
+
+const requestCounterMiddleware = (next: NextFunction) => {
+  requestCounter++;
+  next();
+};
+
 const parserMiddleware = bodyParser({});
 app.use(parserMiddleware);
+app.use(requestCounterMiddleware);
 
 app.use("/addresses", AddressesRouter);
 app.use("/products", ProductRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
